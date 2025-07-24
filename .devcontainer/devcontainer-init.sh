9e12525fc19410821d87de1b2a84f5c8dca7a62b
Jens <jens@boivie.se>
2025-07-24 15:18:28 +0000

    Fixes for development environment

---
+++ .devcontainer/devcontainer-init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+podman pull aap25.containersarelinux.net/ansible-automation-platform-25/ee-supported-rhel9
+podman pull aap25.containersarelinux.net/ansible-automation-platform-25/ee-minimal-utm
