d51b04049a0a677b1308aa99065b740000e7e689
Jens <jens@boivie.se>
2025-07-29 07:04:13 +0000

    Various shit

--- .devcontainer/devcontainer-init.sh
+++ .devcontainer/devcontainer-init.sh
@@ -3,3 +3,4 @@ set -e
 
 podman pull aap25.containersarelinux.net/ansible-automation-platform-25/ee-supported-rhel9
 podman pull aap25.containersarelinux.net/ansible-automation-platform-25/ee-minimal-utm
+podman pull aap25.containersarelinux.net/ansible-automation-platform-25/aap-configurator:latest
---
+++ collections/ansible_collections/jboivie/utm_inventory_plugin/README.md
@@ -0,0 +1,3 @@
+# jboivie.utm_inventory_plugin
+
+This is a custom inventory plugin for UTM virtual machines.
